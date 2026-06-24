b84db6bbe7735bf837371abe3015080f581a672d
linuxct <linuxct@linuxct.space>
2026-06-24 13:54:22 +0200

    chore: Bump to version 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "space.linuxct.pulseloop"
         minSdk = 35
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val localProps = Properties().apply {
             rootProject.file("local.properties").takeIf { it.exists() }?.inputStream()?.use(::load)
